05fd9c19cabe6aff6238b9041e260c62ab81900e
Bhaktij Koli <bhaktijkoli121@gmail.com>
2022-07-20 12:42:27 +0530

    feat: added log to fille

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import os
+import pathlib
 from dotenv import load_dotenv
 from loguru import logger
 from app.mqtt import connect, publish, subscribe
@@ -7,16 +8,22 @@ load_dotenv()
 
 
 def start():
+    logFilePath = (
+        pathlib.Path(__file__).parent.joinpath("..", "logs", "out.log").resolve()
+    )
+    logger.add(logFilePath)
     logger.info("App Started")
 
     ipc_socket = os.getenv("AWS_GG_NUCLEUS_DOMAIN_SOCKET_FILEPATH_FOR_COMPONENT")
     authtoken = os.getenv("SVCUID")
+    stream_port = os.getenv("STREAM_MANAGER_SERVER_PORT")
+    container_authtoken = os.getenv("AWS_CONTAINER_AUTHORIZATION_TOKEN")
     logger.info(f"ipc_socket={ipc_socket}")
     logger.info(f"authtoken={authtoken}")
+    logger.info(f"stream_port={stream_port}")
+    logger.info(f"container_authtoken={container_authtoken}")
 
     connect()
-
-    logger.info("Connected")
 
     subscribe("test/topic")
 
