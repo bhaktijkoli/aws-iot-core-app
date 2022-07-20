56a2579b36ba68e3cee2df6430f4bc592d10cd34
Bhaktij Koli <bhaktijkoli121@gmail.com>
2022-07-21 02:50:49 +0530

    chore: ssl

--- run.py
+++ run.py
@@ -1,6 +1,19 @@
 import os
+import pathlib
 import uvicorn
 from app.main import app
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", "8000")))
+    ssl_certfile = (
+        pathlib.Path(__file__).parent.joinpath("certs", "thingCert.crt").resolve()
+    )
+    ssl_keyfile = (
+        pathlib.Path(__file__).parent.joinpath("certs", "privKey.key").resolve()
+    )
+    uvicorn.run(
+        app,
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", "8000")),
+        ssl_certfile=ssl_certfile,
+        ssl_keyfile=ssl_keyfile,
+    )
--- test.py
+++
@@ -1,5 +0,0 @@
-from datetime import datetime
-
-start_time = datetime.now()
-process_time = (datetime.now() - start_time).microseconds
-print(str(process_time))
